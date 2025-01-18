3647beb8bbd23039cadeb7333e5cae9c06fcf9e4
hungryvito <vsinitsin94@gmail.com>
2025-01-17 23:35:33 -0700

    still trying to fix

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,7 +4,7 @@ import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfil
 
 export default defineConfig({
   plugings: [react()],
-  base: "https://vitalysinitsin.github.io/crime_scan/",
+  base: "/crime_scan/",
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
